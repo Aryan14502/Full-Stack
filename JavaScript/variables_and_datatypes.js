28661cccc095f4a03279db09807bd20006413a1a
Aryan14502 <aryan14502@gmail.com>
2024-12-05 11:24:19 +0530

    JavaScript Datatypes

---
+++ JavaScript/variables_and_datatypes.js
@@ -0,0 +1,17 @@
+const a = "aryan";
+
+console.log(a);
+var c = 30;
+function add(){
+    let a = 10;
+    let b = 20;
+    console.log(a+b);
+    
+    console.log(c);
+}
+
+let b = 10;
+console.log(b);
+
+
+add();
